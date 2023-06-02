b76158e95a5e34d399c952f5275bd1a5d6488d71
Negar Lashgari <negar.lashgari@gmail.com>
2023-06-02 00:16:46 -0400

    Add Student

--- src/main/java/cm/sg/ClassRoster/dao/ClassRosterDao.java
+++ src/main/java/cm/sg/ClassRoster/dao/ClassRosterDao.java
@@ -1,4 +1,20 @@
 package cm.sg.ClassRoster.dao;
 
+import cm.sg.ClassRoster.dto.Student;
+
+import java.util.List;
+
 public interface ClassRosterDao {
+
+    //DAO (Data Access Object) provides methods for performing CRUD (Create, Read, Update, Delete)
+    //all methods by default are abstract in interface.
+
+
+    Student addStudent(String StudentId, Student student);
+
+    Student removeStudent(String StudentId);
+
+    Student ViewStudent(String StudentId);
+
+    List<Student> getAllStudents();
 }

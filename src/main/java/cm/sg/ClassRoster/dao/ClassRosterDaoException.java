bb201b1d26eb9b58cdbb77c13953c2a2036d7e97
Negar Lashgari <negar.lashgari@gmail.com>
2023-06-02 18:04:10 -0400

    Application Exception

---
+++ src/main/java/cm/sg/ClassRoster/dao/ClassRosterDaoException.java
@@ -0,0 +1,13 @@
+package cm.sg.ClassRoster.dao;
+
+public class ClassRosterDaoException extends Exception{  //to handle data access
+
+    //we can declare our own exceptions to handle errors
+    public ClassRosterDaoException(String message) {    //we have two constructors.
+        super(message);   //super indicates we retrieve info from super class=Exception = call the matching constructor on the Exception class by calling super
+    }
+
+    public ClassRosterDaoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
